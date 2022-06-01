f036a980d19036c3c26b2953559c439e1d95ee76
yuk1higa <100584876+yuk1higa@users.noreply.github.com>
2022-06-01 17:37:27 +0900

    ユーザー登録機能の実装

---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+         validates :nickname, presence: :true
+         validates :first_name, presence: :true
+         validates :last_name, presence: :true
+         validates :kana_first_name, presence: :true
+         validates :kana_last_name, presence: :true
+         validates :birthday, presence: :true
+
+end
