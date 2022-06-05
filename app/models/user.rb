2018d4cbcf5b38a0eae54588c370545c2e2def39
yuk1higa <100584876+yuk1higa@users.noreply.github.com>
2022-06-05 21:23:37 +0900

    itemsテーブルの作成とカラムの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         has_many :items
 
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[A-Z])(?=.*?\d)\w{6,}\z/
   validates :password, 
