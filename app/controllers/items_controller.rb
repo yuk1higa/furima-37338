1367d7bdb1827a58fc66d8061b828cc52f75b0b9
yuk1higa <100584876+yuk1higa@users.noreply.github.com>
2022-06-07 07:38:33 +0900

    categoryモデルの作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,5 +5,13 @@ class ItemsController < ApplicationController
   def new
     @item = Item.new
   end
+
+  def create
+    item.create(item_params)
+  end
+
+  private
+  def item_parrams
+    params.require(:item).permit()
   
 end
