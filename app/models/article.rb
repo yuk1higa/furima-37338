8d0cd36ea1d8e2f8b836347415bea6d5353a2185
yuk1higa <100584876+yuk1higa@users.noreply.github.com>
2022-06-15 22:18:07 +0900

    articleモデルのバリデーション追加

--- app/models/article.rb
+++ app/models/article.rb
@@ -5,5 +5,8 @@ class Article < ApplicationRecord
   belongs_to :postage
   belongs_to :area
   belongs_to :schedule
-  
+
+
+  validates :name, :text, :price, :user,  presence: true
+  validates :category_id, :grade_id, :postage_id, :area_id, :area_id, :schedule_id, numericality: { other_than: 1, message: "can't be blank" }
 end
