d4112a1815a30f5aa5048ad868ceea59c4a8d984
yuk1higa <100584876+yuk1higa@users.noreply.github.com>
2022-07-01 18:47:28 +0900

    画像バリデーションの設置とエラーメッセージviewファイルの記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,5 @@
 class Item < ApplicationRecord
-  class Article < ApplicationRecord
+  
     extend ActiveHash::Associations::ActiveRecordExtensions
     belongs_to :category
     belongs_to :grade
@@ -8,9 +8,12 @@ class Item < ApplicationRecord
     belongs_to :schedule
   
   
-    validates :name, :text, :price, :user,  presence: true
+    validates :name, :text, :price, :user, :image,  presence: true
     validates :category_id, :grade_id, :postage_id, :area_id, :area_id, :schedule_id, numericality: { other_than: 1, message: "can't be blank" }
-  end
+    validates :price, numericality: { with: /\A[0-9]+\z/,greater_than: 300, less_than: 9999999, message: '半角数字で300円から9,999,999円の間で出品して下さい'}
+    
+    
+  
   belongs_to :user
   has_one :purchese_record
   has_one_attached :image
