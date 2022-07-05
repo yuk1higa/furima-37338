a85406b8046674d37e5c534944d82a86743650c7
yuk1higa <100584876+yuk1higa@users.noreply.github.com>
2022-07-05 18:52:00 +0900

    rubocopの使用

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :acces_item, only:[:show, :edit, :update, :destroy]
+  before_action :acces_item, only: [:show, :edit, :update, :destroy]
   def index
     @items = Item.all.order('created_at DESC')
   end
@@ -19,7 +19,6 @@ class ItemsController < ApplicationController
   end
 
   def show
-    
   end
 
   def edit
@@ -31,13 +30,12 @@ class ItemsController < ApplicationController
   end
 
   def update
-        @item.update(item_params)
+    @item.update(item_params)
     if @item.valid?
       redirect_to item_path(item_params)
     else
       render 'edit'
     end
-
   end
 
   def destroy
