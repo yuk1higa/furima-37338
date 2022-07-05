77d2457761d29c43b9bef09b1f3928cc2d29ab15
yuk1higa <100584876+yuk1higa@users.noreply.github.com>
2022-07-05 18:51:09 +0900

    ルーティングの設定/コントローラーの設定/show.html.erbファイルの削除リンクの記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :acces_item, only:[:show, :edit, :update]
+  before_action :acces_item, only:[:show, :edit, :update, :destroy]
   def index
     @items = Item.all.order('created_at DESC')
   end
@@ -38,6 +38,15 @@ class ItemsController < ApplicationController
       render 'edit'
     end
 
+  end
+
+  def destroy
+    if @item.user_id == current_user.id
+      @item.destroy
+      redirect_to root_path
+    else
+      redirect_to root_path
+    end
   end
 
   private
