5b8f1842ba78dfcb4b6f5e0f82654fa95c8998fd
yuk1higa <100584876+yuk1higa@users.noreply.github.com>
2022-07-06 22:05:05 +0900

    購入formの作成とバリデーション、保存する処理の記述

---
+++ app/models/purchase_form.rb
@@ -0,0 +1,20 @@
+class PurcheseRecord
+  include ActiveModel::Model
+  attr_accessor :user_id, :item_id, :post_number, :area_id, :municipalitie, :address, :building_name, :phone_number,
+
+  with_options presence: true do
+    validates :user_id
+    validates :item_id
+    validates :post_number, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
+    validates :area_id,numericality: { other_than: 1, message: "can't be blank" }
+    validates :municipalitie
+    validates :address
+    validates :building_name
+    validates :phone_number, format: { with: /\A[0-9]{11}\z/, message: 'is invalid' }
+end
+
+def save
+  purchese_record = PurcheseRecord.create(user_id: user_id, item_id: item_id)
+  Address.create(post_number: post_number, area_id: area_id, municipalitie: municipalitie, address: address, building_name: building_name, phone_number: phone_number )
+  
+end
